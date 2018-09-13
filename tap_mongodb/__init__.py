ec68a27f38db719cecaf979afcbceedb4f093ea6
Chris Capurso <chrisc@stitchdata.com>
2018-09-13 16:45:35 +0000

    full table sync using custom select clause

--- tap_mongodb/__init__.py
+++ tap_mongodb/__init__.py
@@ -1,10 +1,13 @@
+from bson import objectid
+import copy
 import json
 from pymongo import MongoClient
 
 import singer
-from singer import metadata, utils
+from singer import metadata, metrics, utils
 
 import sys
+import time
 
 LOGGER = singer.get_logger()
 
@@ -55,19 +58,101 @@ def do_discover(client):
 
     json.dump({'streams' : streams}, sys.stdout, indent=2)
 
+def get_stream_version(tap_stream_id, state):
+    stream_version = singer.get_bookmark(state, tap_stream_id, 'version')
+
+    if stream_version is None:
+        stream_version = int(time.time() * 1000)
+
+    return stream_version
+
+def row_to_singer_record(stream, row, version, time_extracted):
+    row_to_persist = {}
+
+    import pdb
+    pdb.set_trace
+    1 + 1
+    for column_name, value in row.items():
+        if isinstance(value, objectid.ObjectId):
+            row_to_persist[column_name] = str(value)
+        else:
+            row_to_persist[column_name] = value
+
+    return singer.RecordMessage(
+        stream=stream['stream'],
+        record=row_to_persist,
+        version=version,
+        time_extracted=time_extracted
+    )
+
+
+def do_sync(client, properties, state):
+    streams = properties['streams']
+
+    for stream in streams:
+        mdata = metadata.to_map(stream['metadata'])
+        stream_metadata = mdata.get(())
+
+        select_clause = stream_metadata.get('custom-select-clause')
+
+        if not stream_metadata['selected']:
+            LOGGER.info("Skipping stream {} since it was not selected.".format(stream['tap_stream_id']))
+            continue
+
+        if not select_clause:
+            LOGGER.info("Skipping stream {} since no select clause was provided.".format(stream['tap_stream_id']))
+            continue
+
+        columns = [c.strip(' ') for c in select_clause.split(',')]
+        columns.append('_id')
+
+        db = client[stream_metadata['database-name']]
+        collection = db[stream['stream']]
+
+        singer.write_message(singer.SchemaMessage(
+            stream=stream['stream'],
+            schema=stream['schema'],
+            key_properties=['_id']
+        ))
+
+        with metrics.record_counter(None) as counter:
+            with collection.find() as cursor:
+                rows_saved = 0
+
+                time_extracted = utils.now()
+
+                for row in cursor:
+                    rows_saved += 1
+                    whitelisted_row = {k:v for k,v in row.items() if k in columns}
+                    record_message = row_to_singer_record(stream,
+                                                          whitelisted_row,
+                                                          get_stream_version(stream['tap_stream_id'], state),
+                                                          time_extracted)
+
+                    singer.write_message(record_message)
+
+                    if rows_saved % 1000 == 0:
+                        singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
+
+                singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
+
 def main_impl():
     args = utils.parse_args(REQUIRED_CONFIG_KEYS)
     config = args.config
 
     client =  MongoClient(host=config['host'],
-                          port=config['port'],
+                          port=int(config['port']),
                           username=config['user'],
                           password=config['password'],
-                          authSource=config['database'])
+                          authSource=config['dbname'])
 
     if args.discover:
          do_discover(client)
-
+    elif args.properties:
+        state = args.state or {}
+        do_sync(client, args.properties, state)
+    else:
+        LOGGER.info("No properties were selected")
     # db = client.test
     # foo = db.foo
     # print(foo.find_one())
