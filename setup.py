3dc833fdc4e06c03e6ea0b812d0d2fc00676b198
Andy Lu <andy@stitchdata.com>
2019-11-01 15:32:39 +0000

    Version 1.0.0 and changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-mongodb',
-      version='0.3.0',
+      version='1.0.0',
       description='Singer.io tap for extracting data from MongoDB',
       author='Stitch',
       url='https://singer.io',
