224c4ac3c8169b3e40f4dcf93c2cd622fbbc5083
Nick McCoy <33731945+nick-mccoy@users.noreply.github.com>
2020-02-14 08:40:16 -0500

    bump version to 2.0.0 (#41)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-mongodb',
-      version='1.1.0',
+      version='2.0.0',
       description='Singer.io tap for extracting data from MongoDB',
       author='Stitch',
       url='https://singer.io',
