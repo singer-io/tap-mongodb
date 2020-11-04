995b85c155fbf27548969d04d03aaed191444d3f
Vilson Duka <vilson.duka@datos-intelligence.com>
2020-11-04 11:34:21 +0100

    Add option to use a URI for pymongo connection

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='tap-mongodb',
       py_modules=['tap_mongodb'],
       install_requires=[
           'singer-python==5.8.0',
-          'pymongo==3.8.0',
+          'pymongo[srv]==3.8.0',
           'tzlocal==2.0.0',
           'terminaltables==3.1.0',
       ],
--- tap_mongodb/__init__.py
+++ tap_mongodb/__init__.py
@@ -350,31 +350,43 @@ def do_sync(client, catalog, state):
 
 
 def main_impl():
-    args = utils.parse_args(REQUIRED_CONFIG_KEYS)
+
+    # Check if we use DNS string or not
+    args = utils.parse_args([])
     config = args.config
 
-    # Default SSL verify mode to true, give option to disable
-    verify_mode = config.get('verify_mode', 'true') == 'true'
-    use_ssl = config.get('ssl') == 'true'
+    if 'connection_uri' in config.keys():
+        # Connect using the DNS string
+        client = pymongo.MongoClient(config['connection_uri'])
+        LOGGER.info('Connected to MongoDB host: %s, version: %s',
+                    config['connection_uri'],
+                    client.server_info().get('version', 'unknown'))
+    else:
+        # Connect using the connection parameters
+        args = utils.parse_args(REQUIRED_CONFIG_KEYS)
+        config = args.config
 
-    connection_params = {"host": config['host'],
-                         "port": int(config['port']),
-                         "username": config.get('user', None),
-                         "password": config.get('password', None),
-                         "authSource": config['database'],
-                         "ssl": use_ssl,
-                         "replicaset": config.get('replica_set', None),
-                         "readPreference": 'secondaryPreferred'}
+        # Default SSL verify mode to true, give option to disable
+        verify_mode = config.get('verify_mode', 'true') == 'true'
+        use_ssl = config.get('ssl') == 'true'
 
-    # NB: "ssl_cert_reqs" must ONLY be supplied if `SSL` is true.
-    if not verify_mode and use_ssl:
-        connection_params["ssl_cert_reqs"] = ssl.CERT_NONE
+        connection_params = {"host": config['host'],
+                             "port": int(config['port']),
+                             "username": config.get('user', None),
+                             "password": config.get('password', None),
+                             "authSource": config['database'],
+                             "ssl": use_ssl,
+                             "replicaset": config.get('replica_set', None),
+                             "readPreference": 'secondaryPreferred'}
 
-    client = pymongo.MongoClient(**connection_params)
+        # NB: "ssl_cert_reqs" must ONLY be supplied if `SSL` is true.
+        if not verify_mode and use_ssl:
+            connection_params["ssl_cert_reqs"] = ssl.CERT_NONE
+        client = pymongo.MongoClient(**connection_params)
 
-    LOGGER.info('Connected to MongoDB host: %s, version: %s',
-                config['host'],
-                client.server_info().get('version', 'unknown'))
+        LOGGER.info('Connected to MongoDB host: %s, version: %s',
+                    config['host'],
+                    client.server_info().get('version', 'unknown'))
 
     common.INCLUDE_SCHEMAS_IN_DESTINATION_STREAM_NAME = \
         (config.get('include_schemas_in_destination_stream_name') == 'true')
