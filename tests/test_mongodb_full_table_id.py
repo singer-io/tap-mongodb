5ab5eb4ace1360915a800962ebdd8504b7c7a21e
Kyle Speer <kspeer@stitchdata.com>
2020-12-17 20:17:11 +0000

    make tap_name consistent across tests, cleanup config

--- tests/test_mongodb_full_table_id.py
+++ tests/test_mongodb_full_table_id.py
@@ -99,7 +99,7 @@ class MongoDBFullTableID(unittest.TestCase):
         return "tap_tester_mongodb_full_table_id"
 
     def tap_name(self):
-        return "tap_tester_mongodb_full_table_id"
+        return "tap-mongodb"
 
     def get_type(self):
         return "platform.mongodb"
