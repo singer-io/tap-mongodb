b4044ce66e18d235be6e0c84645477bc78b86420
Harsh <80324346+harshpatel4crest@users.noreply.github.com>
2022-07-01 19:00:38 +0530

    TDL-19353: pymongo driver issue (#81)
    * upgraded pymongo version to 3.12.3
    
    * Bump to v2.1.2 and update changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-mongodb',
-      version='2.1.1',
+      version='2.1.2',
       description='Singer.io tap for extracting data from MongoDB',
       author='Stitch',
       url='https://singer.io',
@@ -11,7 +11,7 @@ setup(name='tap-mongodb',
       py_modules=['tap_mongodb'],
       install_requires=[
           'singer-python==5.8.0',
-          'pymongo==3.8.0',
+          'pymongo==3.12.3',
           'tzlocal==2.0.0',
           'terminaltables==3.1.0',
       ],
