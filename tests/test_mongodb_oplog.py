e7fa8ed514ba68ea334e91b5af7f9bfe8aacbc09
Manoj Kumar Anand <manand@talend.com>
2022-09-20 13:50:48 +0000

    modified test to isolate transactions

--- tests/test_mongodb_oplog.py
+++ tests/test_mongodb_oplog.py
@@ -4,22 +4,10 @@ import tap_tester.runner      as runner
 import os
 import datetime
 import unittest
-import datetime
-import pymongo
 import string
 import random
-import time
-import re
-import pprint
-import pdb
-import bson
 from bson import ObjectId
-import singer
-from functools import reduce
-from singer import utils, metadata
 from mongodb_common import drop_all_collections, get_test_connection, ensure_environment_variables_set
-import decimal
-
 
 RECORD_COUNT = {}
 
@@ -27,11 +15,13 @@ RECORD_COUNT = {}
 def random_string_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for x in range(size))
 
+
 def generate_simple_coll_docs(num_docs):
     docs = []
     for int_value in range(num_docs):
         docs.append({"int_field": int_value, "string_field": random_string_generator()})
     return docs
+
 
 class MongoDBOplog(unittest.TestCase):
     def setUp(self):
@@ -48,9 +38,6 @@ class MongoDBOplog(unittest.TestCase):
 
             # simple_coll_2 has 100 documents
             client["simple_db"]["simple_coll_2"].insert_many(generate_simple_coll_docs(100))
-
-
-
 
     def expected_check_streams(self):
         return {
@@ -69,7 +56,6 @@ class MongoDBOplog(unittest.TestCase):
             'simple_coll_1': 50,
             'simple_coll_2': 100,
         }
-
 
     def expected_sync_streams(self):
         return {
@@ -90,12 +76,12 @@ class MongoDBOplog(unittest.TestCase):
         return {'password': os.getenv('TAP_MONGODB_PASSWORD')}
 
     def get_properties(self):
-        return {'host' : os.getenv('TAP_MONGODB_HOST'),
-                'port' : os.getenv('TAP_MONGODB_PORT'),
-                'user' : os.getenv('TAP_MONGODB_USER'),
-                'database' : os.getenv('TAP_MONGODB_DBNAME')
-        }
-
+        return {'host': os.getenv('TAP_MONGODB_HOST'),
+                'port': os.getenv('TAP_MONGODB_PORT'),
+                'user': os.getenv('TAP_MONGODB_USER'),
+                'database': os.getenv('TAP_MONGODB_DBNAME')
+                # 'replica_set': 'test_for_user'
+                }
 
     def test_run(self):
 
@@ -118,8 +104,6 @@ class MongoDBOplog(unittest.TestCase):
         # assert we find the correct streams
         self.assertEqual(self.expected_check_streams(),
                          {c['tap_stream_id'] for c in found_catalogs})
-
-
 
         for tap_stream_id in self.expected_check_streams():
             found_stream = [c for c in found_catalogs if c['tap_stream_id'] == tap_stream_id][0]
@@ -178,60 +162,29 @@ class MongoDBOplog(unittest.TestCase):
             self.assertIsNotNone(state['bookmarks'][tap_stream_id]['oplog_ts_time'])
             self.assertIsNotNone(state['bookmarks'][tap_stream_id]['oplog_ts_inc'])
 
+        ################ Isolating Deletes #####################
 
-        changed_ids = set()
+        changed_ids_deletes = set()
         with get_test_connection() as client:
             # Delete two documents for each collection
 
-            changed_ids.add(client['simple_db']['simple_coll_1'].find({'int_field': 0})[0]['_id'])
+            changed_ids_deletes.add(client['simple_db']['simple_coll_1'].find({'int_field': 0})[0]['_id'])
             client["simple_db"]["simple_coll_1"].delete_one({'int_field': 0})
 
-            changed_ids.add(client['simple_db']['simple_coll_1'].find({'int_field': 1})[0]['_id'])
+            changed_ids_deletes.add(client['simple_db']['simple_coll_1'].find({'int_field': 1})[0]['_id'])
             client["simple_db"]["simple_coll_1"].delete_one({'int_field': 1})
 
-            changed_ids.add(client['simple_db']['simple_coll_2'].find({'int_field': 0})[0]['_id'])
+            changed_ids_deletes.add(client['simple_db']['simple_coll_2'].find({'int_field': 0})[0]['_id'])
             client["simple_db"]["simple_coll_2"].delete_one({'int_field': 0})
 
-            changed_ids.add(client['simple_db']['simple_coll_2'].find({'int_field': 1})[0]['_id'])
+            changed_ids_deletes.add(client['simple_db']['simple_coll_2'].find({'int_field': 1})[0]['_id'])
             client["simple_db"]["simple_coll_2"].delete_one({'int_field': 1})
 
-            # Update two documents for each collection
-            changed_ids.add(client['simple_db']['simple_coll_1'].find({'int_field': 48})[0]['_id'])
-            client["simple_db"]["simple_coll_1"].update_one({'int_field': 48},{'$set': {'int_field': -1}})
-
-            changed_ids.add(client['simple_db']['simple_coll_1'].find({'int_field': 49})[0]['_id'])
-            client["simple_db"]["simple_coll_1"].update_one({'int_field': 49},{'$set': {'int_field': -1}})
-
-            changed_ids.add(client['simple_db']['simple_coll_2'].find({'int_field': 98})[0]['_id'])
-            client["simple_db"]["simple_coll_2"].update_one({'int_field': 98},{'$set': {'int_field': -1}})
-
-            changed_ids.add(client['simple_db']['simple_coll_2'].find({'int_field': 99})[0]['_id'])
-            client["simple_db"]["simple_coll_2"].update_one({'int_field': 99},{'$set': {'int_field': -1}})
-
-            # Insert two documents for each collection
-            client["simple_db"]["simple_coll_1"].insert_one({"int_field": 50, "string_field": random_string_generator()})
-            changed_ids.add(client['simple_db']['simple_coll_1'].find({'int_field': 50})[0]['_id'])
-
-            client["simple_db"]["simple_coll_1"].insert_one({"int_field": 51, "string_field": random_string_generator()})
-            changed_ids.add(client['simple_db']['simple_coll_1'].find({'int_field': 51})[0]['_id'])
-
-            client["simple_db"]["simple_coll_2"].insert_one({"int_field": 100, "string_field": random_string_generator()})
-            changed_ids.add(client['simple_db']['simple_coll_2'].find({'int_field': 100})[0]['_id'])
-
-            client["simple_db"]["simple_coll_2"].insert_one({"int_field": 101, "string_field": random_string_generator()})
-            changed_ids.add(client['simple_db']['simple_coll_2'].find({'int_field': 101})[0]['_id'])
-
-        #  -----------------------------------
-        # ----------- Subsequent Oplog Sync ---------
-        #  -----------------------------------
-
-        # Run sync
-
+        # Run sync and verify the deletes
         sync_job_name = runner.run_sync_mode(self, conn_id)
 
         exit_status = menagerie.get_exit_status(conn_id, sync_job_name)
         menagerie.verify_sync_exit_status(self, exit_status, sync_job_name)
-
 
         # verify the persisted schema was correct
         messages_by_stream = runner.get_records_from_target_output()
@@ -239,6 +192,59 @@ class MongoDBOplog(unittest.TestCase):
         for stream_name in self.expected_sync_streams():
             records_by_stream[stream_name] = [x for x in messages_by_stream[stream_name]['messages'] if x.get('action') == 'upsert']
 
+        # assert that each of the streams that we synced are the ones that we expect to see
+        record_count_by_stream = runner.examine_target_output_file(self,
+                                                                   conn_id,
+                                                                   self.expected_sync_streams(),
+                                                                   self.expected_pks())
+
+        # Verify that we got at least 2 records due to deletes
+        # (could be more due to overlap in gte oplog clause)
+        for k, v in record_count_by_stream.items():
+            self.assertGreaterEqual(v, 2)
+
+        # Verify that we got 2 records with _SDC_DELETED_AT
+        self.assertEqual(2, len([x['data'] for x in records_by_stream['simple_coll_1'] if x['data'].get('_sdc_deleted_at')]))
+        self.assertEqual(2, len([x['data'] for x in records_by_stream['simple_coll_2'] if x['data'].get('_sdc_deleted_at')]))
+
+        # gather the actual _ids from the replicated data
+        actual = set([ObjectId(x['data']['_id']) for x in records_by_stream['simple_coll_1']]).union(set([ObjectId(x['data']['_id']) for x in records_by_stream['simple_coll_2']]))
+
+        # since there is an overlap in log based we need to validate if there is only 1 extra _id of the overlapping document in the actual
+        self.assertGreaterEqual(len(actual), 4)
+
+        # Verify that the changed_ids is a subset of the actual _ids
+        self.assertTrue(changed_ids_deletes.issubset(actual))
+
+        ################ Isolating Updates #################
+
+        changed_ids_updates = set()
+        with get_test_connection() as client:
+
+            # Update two documents for each collection
+            changed_ids_updates.add(client['simple_db']['simple_coll_1'].find({'int_field': 48})[0]['_id'])
+            client["simple_db"]["simple_coll_1"].update_one({'int_field': 48},{'$set': {'int_field': -1}})
+
+            changed_ids_updates.add(client['simple_db']['simple_coll_1'].find({'int_field': 49})[0]['_id'])
+            client["simple_db"]["simple_coll_1"].update_one({'int_field': 49},{'$set': {'int_field': -1}})
+
+            changed_ids_updates.add(client['simple_db']['simple_coll_2'].find({'int_field': 98})[0]['_id'])
+            client["simple_db"]["simple_coll_2"].update_one({'int_field': 98},{'$set': {'int_field': -1}})
+
+            changed_ids_updates.add(client['simple_db']['simple_coll_2'].find({'int_field': 99})[0]['_id'])
+            client["simple_db"]["simple_coll_2"].update_one({'int_field': 99},{'$set': {'int_field': -1}})
+
+        # Run sync and verify the updates
+        sync_job_name = runner.run_sync_mode(self, conn_id)
+
+        exit_status = menagerie.get_exit_status(conn_id, sync_job_name)
+        menagerie.verify_sync_exit_status(self, exit_status, sync_job_name)
+
+        # verify the persisted schema was correct
+        messages_by_stream = runner.get_records_from_target_output()
+        records_by_stream = {}
+        for stream_name in self.expected_sync_streams():
+            records_by_stream[stream_name] = [x for x in messages_by_stream[stream_name]['messages'] if x.get('action') == 'upsert']
 
         # assert that each of the streams that we synced are the ones that we expect to see
         record_count_by_stream = runner.examine_target_output_file(self,
@@ -246,15 +252,66 @@ class MongoDBOplog(unittest.TestCase):
                                                                    self.expected_sync_streams(),
                                                                    self.expected_pks())
 
-        # Verify that we got at least 6 records due to changes
+        # Verify that we got at least 2 records due to updates
         # (could be more due to overlap in gte oplog clause)
-        for k,v in record_count_by_stream.items():
-            self.assertGreaterEqual(v, 6)
+        for k, v in record_count_by_stream.items():
+            self.assertGreaterEqual(v, 2)
 
-        # Verify that we got 2 records with _SDC_DELETED_AT
-        self.assertEqual(2, len([x['data'] for x in records_by_stream['simple_coll_1'] if x['data'].get('_sdc_deleted_at')]))
-        self.assertEqual(2, len([x['data'] for x in records_by_stream['simple_coll_2'] if x['data'].get('_sdc_deleted_at')]))
-        # Verify that the _id of the records sent are the same set as the
-        # _ids of the documents changed
+        # gather the actual _ids from the replicated data
         actual = set([ObjectId(x['data']['_id']) for x in records_by_stream['simple_coll_1']]).union(set([ObjectId(x['data']['_id']) for x in records_by_stream['simple_coll_2']]))
-        self.assertEqual(changed_ids, actual)
+
+        # since there is an overlap in log based we need to validate if there is only 1 extra _id of the overlapping document in the actual
+        self.assertEqual(len(actual), 5)
+
+        # Verify that the changed_ids is a subset of the actual _ids
+        self.assertTrue(changed_ids_updates.issubset(actual))
+
+        ########### Isolating inserts #############
+
+        changed_ids_inserts = set()
+        with get_test_connection() as client:
+
+            # Insert two documents for each collection
+            client["simple_db"]["simple_coll_1"].insert_one({"int_field": 50, "string_field": random_string_generator()})
+            changed_ids_inserts.add(client['simple_db']['simple_coll_1'].find({'int_field': 50})[0]['_id'])
+
+            client["simple_db"]["simple_coll_1"].insert_one({"int_field": 51, "string_field": random_string_generator()})
+            changed_ids_inserts.add(client['simple_db']['simple_coll_1'].find({'int_field': 51})[0]['_id'])
+
+            client["simple_db"]["simple_coll_2"].insert_one({"int_field": 100, "string_field": random_string_generator()})
+            changed_ids_inserts.add(client['simple_db']['simple_coll_2'].find({'int_field': 100})[0]['_id'])
+
+            client["simple_db"]["simple_coll_2"].insert_one({"int_field": 101, "string_field": random_string_generator()})
+            changed_ids_inserts.add(client['simple_db']['simple_coll_2'].find({'int_field': 101})[0]['_id'])
+
+        # Run sync and verify the inserts
+        sync_job_name = runner.run_sync_mode(self, conn_id)
+
+        exit_status = menagerie.get_exit_status(conn_id, sync_job_name)
+        menagerie.verify_sync_exit_status(self, exit_status, sync_job_name)
+
+        # verify the persisted schema was correct
+        messages_by_stream = runner.get_records_from_target_output()
+        records_by_stream = {}
+        for stream_name in self.expected_sync_streams():
+            records_by_stream[stream_name] = [x for x in messages_by_stream[stream_name]['messages'] if x.get('action') == 'upsert']
+
+        # assert that each of the streams that we synced are the ones that we expect to see
+        record_count_by_stream = runner.examine_target_output_file(self,
+                                                                   conn_id,
+                                                                   self.expected_sync_streams(),
+                                                                   self.expected_pks())
+
+        # Verify that we got at least 2 records due to inserts
+        # (could be more due to overlap in gte oplog clause)
+        for k, v in record_count_by_stream.items():
+            self.assertGreaterEqual(v, 2)
+
+        # gather the actual _ids from the replicated data
+        actual = set([ObjectId(x['data']['_id']) for x in records_by_stream['simple_coll_1']]).union(set([ObjectId(x['data']['_id']) for x in records_by_stream['simple_coll_2']]))
+
+        # since there is an overlap in log based we need to validate if there is only 1 extra _id of the overlapping document in the actual
+        self.assertEqual(len(actual), 5)
+
+        # Verify that the changed_ids is a subset of the actual _ids
+        self.assertTrue(changed_ids_inserts.issubset(actual))
