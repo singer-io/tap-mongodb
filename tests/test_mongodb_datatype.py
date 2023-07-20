ca5f5dd4bc9f10386dcddb2f427daf6a30e1c293
RushiT0122 <rtodkar@stitchdata-talend.com>
2023-07-20 08:13:25 +0000

    fix integration test compatibility issues with pymongo v4.3+ and mongoddb v6.0

--- tests/test_mongodb_datatype.py
+++ tests/test_mongodb_datatype.py
@@ -14,14 +14,16 @@ from tap_tester import connections, menagerie, runner
 RECORD_COUNT = {}
 
 
-def run_mongodb_javascript(database, js):
+def run_mongodb_javascript(database, js, mongo_version):
     """
     Runs arbitrary javascript against the test Mongo instance. This is
     useful for setting up situations that Python can't handle (e.g.,
     datetime with year 0) for testing.
     """
     print("Running '{}' against database '{}'".format(js, database))
-    cmd = ["mongo", "-u", os.getenv('TAP_MONGODB_USER'), "-p", os.getenv('TAP_MONGODB_PASSWORD'), "--authenticationDatabase", os.getenv('TAP_MONGODB_DBNAME'), database, "--eval", "eval('{}')".format(js)]
+
+    mongo_shell = "mongosh" if int(mongo_version.split(".")[0]) > 5 else "mongo"
+    cmd = [mongo_shell, "-u", os.getenv('TAP_MONGODB_USER'), "-p", os.getenv('TAP_MONGODB_PASSWORD'), "--authenticationDatabase", os.getenv('TAP_MONGODB_DBNAME'), database, "--eval", "eval('{}')".format(js)]
     subprocess.run(cmd)
 
 
@@ -74,7 +76,10 @@ class MongoDBDatatype(unittest.TestCase):
             client["datatype_db"]["datatype_coll_1"].insert_one(datatype_doc)
 
             # NB: Insert an invalid datetime to confirm that works correctly
-            run_mongodb_javascript("datatype_db", "db.invalid_datatype_coll.insert({ \"date_field\": new ISODate(\"0000-01-01T00:00:00.000Z\") });")
+            mongodb_version = client.server_info()["version"]
+            run_mongodb_javascript(database="datatype_db",
+                                   js="db.invalid_datatype_coll.insert({ \"date_field\": new ISODate(\"0000-01-01T00:00:00.000Z\") });",
+                                   mongo_version=mongodb_version)
 
     def expected_check_streams(self):
         return {
